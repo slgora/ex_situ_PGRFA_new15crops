158d6a737cebd7f2a09facc2c17498e457a35357
Sarah Gora <sarahluciagora@gmail.com>
2025-09-29 14:07:23 -0600

    Updated load_SGSV_data function for the SGSV data for new 15 crops
    Refactor load_SGSV_data function to read and process SGSV data for new 15 crops

--- Functions/Load_SGSV_data.R
+++ Functions/Load_SGSV_data.R
@@ -1,15 +1,43 @@
-# library(readxl) us needed
-load_SGSV_data = function(filepath){
+##' load_SGSV_data
+#'
+#' Loads and formats SGSV data for the 15 crops from the Svalbard Global Seed Vault portal (by genus).
+#' Expects columns: "Institute name", "Institute code", "Institute acronym", "Accession number",
+#' "Full scientific name", "Species", "Country of collection", "source_file".
+#' Output: INSTCODE, instAcronym, ACCENUMB, fullTaxa, GENUS, SPECIES, origctyFullName, ID.
+#'
+#' For SPECIES, only the second word in 'fullTaxa' is used.
+#'
+#' @param filepath Path to the Excel file.
+#' @return Data frame with standardized and parsed columns.
+#' @examples
+#' sgsv <- load_SGSV_data("SGSV_new15crops_unformatted.xlsx")
+load_SGSV_data <- function(filepath){
+  library(readxl)
+  library(dplyr)
+  library(stringr)
+
   SGSV_allcrops <- read_excel(filepath)
-  # Change column names
-  colnames(SGSV_allcrops) <- c( "source","instName","INSTCODE","instAcronym","ACCENUMB",
-                                "fullTaxa","GENUS_SPECIES","origctyFullName") #origCtyFullName
-  ## Keep fields
-  SGSV_allcrops <- subset(SGSV_allcrops, select = c(INSTCODE, instAcronym, 
-                                                    ACCENUMB, fullTaxa, GENUS_SPECIES, 
-                                                    origctyFullName))
-  # create SPECIES and GENUS field
-  SGSV_allcrops  <- SGSV_allcrops  %>% separate(GENUS_SPECIES, c('GENUS', 'SPECIES'))
+  # Assign column names matching the input file structure
+  colnames(SGSV_allcrops) <- c(
+    "instName",         # Institute name
+    "INSTCODE",         # Institute code
+    "instAcronym",      # Institute acronym
+    "ACCENUMB",         # Accession number
+    "fullTaxa",         # Full scientific name
+    "Species",          # Species (not used, will be parsed from fullTaxa)
+    "origctyFullName",  # Country of collection
+    "source_file"       # Source file
+  )
+  # Select and parse columns
+  SGSV_allcrops <- SGSV_allcrops %>%
+    select(INSTCODE, instAcronym, ACCENUMB, fullTaxa, origctyFullName) %>%
+    mutate(
+      GENUS = word(fullTaxa, 1),
+      SPECIES = word(fullTaxa, 2),
+      ID = paste0(ACCENUMB, INSTCODE)
+    ) %>%
+    select(INSTCODE, instAcronym, ACCENUMB, fullTaxa, GENUS, SPECIES, origctyFullName, ID)
+
   return(SGSV_allcrops)
 }
 
